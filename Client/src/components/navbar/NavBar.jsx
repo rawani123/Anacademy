16e21b6c26c0685d7e039a17da1a88efb6e1d38e
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-05-03 15:40:29 +0530

    color changed

--- Client/src/components/navbar/NavBar.jsx
+++ Client/src/components/navbar/NavBar.jsx
@@ -14,21 +14,22 @@ function classNames(...classes) {
 
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-[#012641]">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <Link to="/" className="flex flex-shrink-0 items-center text-purple-600 text-3xl italic hover:text-purple-600">
+                <Link to="/" className="flex flex-shrink-0 items-center text-white text-3xl italic hover:text-white
+                ">
                   Anacademy
                 </Link>
               </div>
               <div className="flex gap-5 text-white">
-                <Link to="signup" className=' border-purple-600 border-2 hover:bg-purple-500 hover:text-white hover:shadow-white hover:shadow-md py-2 px-5 rounded-3xl'>
+                <Link to="signup" className=' border-purple-600 border-2 hover:bg-[#60B3D1] hover:text-white hover:shadow-white hover:shadow-md py-2 px-5 rounded-3xl'>
                     Signup
                 </Link>
-                <Link to="login" className=' border-purple-600 hover:bg-purple-500 hover:text-white hover:shadow-white hover:shadow-md border-2 py-2 px-14 rounded-3xl'>
+                <Link to="login" className=' border-purple-600 hover:bg-[#60B3D1] hover:text-white hover:shadow-white hover:shadow-md border-2 py-2 px-14 rounded-3xl'>
                     Login
                 </Link>
               </div>
